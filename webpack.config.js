45a43f466029ab4e6f4956f5df30a18ebf6369f1
Eoin O'Brien <eoinobrien910@gmail.com>
2017-12-04 01:28:41 +0000

    build(webpack): reformat and lint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,60 @@
-var nodeExternals = require('webpack-node-externals');
-var webpack = require('webpack');
-var browserify = require('browserify');
-var path = require('path');
-var fs = require('fs');
-var os = require('os');
-var deleteEmpty = require('delete-empty');
-var PACKAGE_FILE = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8'));
-var LIB_NAME = PACKAGE_FILE.name;
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+const browserify = require('browserify');
+const path = require('path');
+const fs = require('fs');
+
+
+const PACKAGE_FILE = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8').toString());
+const LIB_NAME = PACKAGE_FILE.name;
 
 /* helper function to get into build directory */
-var libPath = function(name) {
-  if ( undefined === name ) {
+const libPath = function (name) {
+  if (undefined === name) {
     return 'dist';
   }
 
   return path.join('dist', name);
-}
+};
 
 /* helper to clean leftovers */
-var outputCleanup = function(dir) {
-  if (false == fs.existsSync(libPath())){
+const outputCleanup = function (dir) {
+  if (!fs.existsSync(libPath())) {
     return;
   }
 
-  var list = fs.readdirSync(dir);
-  for(var i = 0; i < list.length; i++) {
-    var filename = path.join(dir, list[i]);
-    var stat = fs.statSync(filename);
+  const list = fs.readdirSync(dir);
+  for (let i = 0; i < list.length; i++) {
+    const filename = path.join(dir, list[i]);
+    const stat = fs.statSync(filename);
 
-    if(filename == '.' || filename == '..') {
+    if (filename === '.' || filename === '..') {
       // pass these files
-      } else if(stat.isDirectory()) {
-        // outputCleanup recursively
-        outputCleanup(filename, false);
-      } else {
-        // rm fiilename
-        fs.unlinkSync(filename);
-      }
+    } else if (stat.isDirectory()) {
+      // outputCleanup recursively
+      outputCleanup(filename, false);
+    } else {
+      // rm fiilename
+      fs.unlinkSync(filename);
+    }
   }
   fs.rmdirSync(dir);
 };
 
-/* precentage handler is used to hook build start and ending */
-var percentage_handler = function handler(percentage, msg) {
-  if ( 0 === percentage ) {
+/* percentage handler is used to hook build start and ending */
+const percentage_handler = function handler(percentage) {
+  if (0 === percentage) {
     /* Build Started */
     outputCleanup(libPath());
     console.log('Build started... Good luck!');
-  } else if ( 1.0 === percentage ) {
+  } else if (1.0 === percentage) {
     // TODO: No Error detection. :(
     create_browser_version(webpack_opts.output.filename);
   }
-}
+};
 
-var webpack_opts = {
+// noinspection JSUnresolvedFunction
+const webpack_opts = {
   entry: './src/index.ts',
   target: 'node',
   output: {
@@ -64,7 +65,7 @@ var webpack_opts = {
     extensions: ['.ts', '.js'],
     modules: [
       'node_modules',
-      'src',
+      'src'
     ]
   },
   module: {
@@ -73,15 +74,15 @@ var webpack_opts = {
         enforce: 'pre',
         test: /\.ts$/,
         loader: 'tslint-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       }, {
         test: /\.ts$/,
         loader: 'ts-loader',
         exclude: [
           /node_modules/
-        ],
-      },
-    ],
+        ]
+      }
+    ]
   },
   externals: [nodeExternals()],
   plugins: [
@@ -96,9 +97,9 @@ var webpack_opts = {
     }),
     new webpack.ProgressPlugin(percentage_handler)
   ],
-}
+};
 
-var create_browser_version = function (inputJs) {
+const create_browser_version = function (inputJs) {
   let outputName = inputJs.replace(/\.[^/.]+$/, '');
   outputName = `${outputName}.browser.js`;
   console.log('Creating browser version ...');
@@ -107,12 +108,13 @@ var create_browser_version = function (inputJs) {
     standalone: LIB_NAME,
   });
 
-  b.bundle(function(err, src) {
-    if ( err != null ) {
+  // noinspection JSUnresolvedFunction
+  b.bundle(function (err) {
+    if (err != null) {
       console.error('Browserify error:');
       console.error(err);
     }
   }).pipe(fs.createWriteStream(outputName));
-}
+};
 
 module.exports = webpack_opts;
