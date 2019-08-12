a9339d10170e86e9aceeb26203ba214483dd656e
Eoin O'Brien <eoinobrien910@gmail.com>
2019-08-12 03:40:05 +0100

    test: use randomized MongoDB databases for each test script

---
+++ __tests__/__setup__.js
@@ -0,0 +1,24 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import mongoose from 'mongoose';
+import uuidv4 from 'uuid/v4';
+
+// Configure mongoose to use the global promise library
+mongoose.Promise = Promise;
+
+// Connect to MongoDB before testing
+beforeAll(async () => {
+  // Connect to a unique database per test script
+  const mongoDbUri = 'mongodb://localhost/mongoose-update-if-current-' + uuidv4();
+  const connectionOptions = {
+    useNewUrlParser: true,
+  };
+  await mongoose.connect(mongoDbUri, connectionOptions);
+});
+
+// Drop database and disconnect from MongoDB after testing
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.disconnect();
+});
