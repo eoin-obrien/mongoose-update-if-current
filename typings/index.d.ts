37f17993adc07e8abd9ff3302a4dd14c82f32fa1
derevi <derevi@>
2021-08-05 16:35:05 -0400

    Fixed issue #386 added generic to function type declaration of updateIfCurrentPlugin

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,6 @@
 import { Schema } from 'mongoose';
 
-export declare function updateIfCurrentPlugin(schema: Schema, options?: PluginOptions): void;
+export declare function updateIfCurrentPlugin<T>(schema: Schema<T>, options?: PluginOptions): void;
 
 export declare interface PluginOptions {
   strategy?: 'version' | 'timestamp';
